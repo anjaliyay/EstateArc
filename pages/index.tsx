190aa3aa79ebe7a8abcd32c55724f3e852ab6c50
anjaliyay <95963877+anjaliyay@users.noreply.github.com>
2023-04-23 01:40:06 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>OneRoof</title>
+        <title>EstateArc</title>
         <meta name="description" content="Finding your next home made easy!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
